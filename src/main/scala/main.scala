d976749eca33ed15a79e7253327b79ea1444af96
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-10-28 15:35:22 +0400

    Score for one fast answer tests

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -13,4 +13,29 @@ enum Difficulty {
 
 case class Answer(isCorrect: Boolean, time: Double, wordsCountInQuestion: Int)
 
-def score(answers: List[Answer]): Int = 0
+def score(answers: List[Answer]): Int = {
+  answers.foldLeft(0)((ac, answer) =>
+    if (isCorrectAndFast(answer)) {
+      scoreForCorrectAndFast(answer)
+    } else {
+      0
+    }
+  )
+}
+
+def scoreForCorrectAndFast(answer: Answer) = {
+  val remainingTimeUntilAnswerIsNotFast = maxTimeThatIsConsideredFast(answer) - answer.time
+  (3 * remainingTimeUntilAnswerIsNotFast + 3).toInt
+}
+
+def isCorrectAndFast(answer: Answer): Boolean = {
+  answer.isCorrect && isFast(answer)
+}
+
+def isFast(answer: Answer): Boolean = {
+  answer.time <= maxTimeThatIsConsideredFast(answer)
+}
+
+def maxTimeThatIsConsideredFast(answer: Answer): Double = {
+  answer.wordsCountInQuestion.toDouble / 4 + 3
+}
--- src/test/scala/ScoreTests.scala
+++ src/test/scala/ScoreTests.scala
@@ -11,6 +11,21 @@ class ScoreTests extends AnyFunSuite {
     assert(score(answers) == 0)
   }
 
+  test("Score for one answer which has been solved instantaneously") {
+    val answers = List(Answer(isCorrect = true, time = 0, wordsCountInQuestion = 16))
+    assert(score(answers) == 24)
+  }
+
+  test("Score for one answer which has been solved after 1 sec") {
+    val answers = List(Answer(isCorrect = true, time = 1, wordsCountInQuestion = 16))
+    assert(score(answers) == 21)
+  }
+
+  test("Score for one answer which has been solved after maximum time that is considered fast") {
+    val answers = List(Answer(isCorrect = true, time = 7, wordsCountInQuestion = 16))
+    assert(score(answers) == 3)
+  }
+
   private def anyTime(): Double = 0.0
   private def anyWordCount(): Int = 0
 }
