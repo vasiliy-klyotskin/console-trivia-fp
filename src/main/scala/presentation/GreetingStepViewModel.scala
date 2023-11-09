001ad5248134a3a9c42867b8e45e68ea86ed4d25
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-11-09 12:29:22 +0400

    greeting step added

---
+++ src/main/scala/presentation/GreetingStepViewModel.scala
@@ -0,0 +1,7 @@
+package org.kyotskin.trivia
+package presentation
+
+case class GreetingStepViewModel(
+  greeting: String,
+  nameRequest: String
+)
--- src/main/scala/presentation/Mapping.scala
+++ src/main/scala/presentation/Mapping.scala
@@ -21,3 +21,9 @@ def mapToQuestionViewModel(question: Question): QuestionViewModel = {
     .map((ans, index) => s"${index+1}. $ans")
   QuestionViewModel(question.text, possibleAnswers)
 }
+
+def greetingStep(): GreetingStepViewModel = {
+  val greeting = "Welcome to the Console Trivia Game!\n\nGet ready to test your knowledge and have a blast with our trivia questions. Whether you're a seasoned trivia pro or just looking for some fun, this game is perfect for everyone. Answer questions from a variety of categories, rack up points, and compete with friends or challenge yourself.\n\nLet's see how much you know and how well you can think on your feet. Are you ready to embark on this thrilling trivia adventure? Good luck and have a fantastic time!"
+  val nameRequest = "Before we begin, please enter your name so we can personalize your experience and keep track of your score. Type your name and press Enter to get started."
+  GreetingStepViewModel(greeting, nameRequest)
+}
