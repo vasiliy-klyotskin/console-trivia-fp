ab8c91bd7ac6f4e0ecdec20e5e280d1cef2d6112
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-12-11 14:57:57 +0800

    There are incorrect answers in dto test

--- src/main/scala/networking/QuestionRemoteMapping.scala
+++ src/main/scala/networking/QuestionRemoteMapping.scala
@@ -1,7 +1,15 @@
 package org.kyotskin.trivia
 package networking
 import domain.Question
+import cats.effect.IO
 
-def map(remote: QuestionRemoteDTO): Question = {
-  Question(text = remote.question.text, possibleAnswers = List(remote.correctAnswer), correctAnswer = remote.correctAnswer)
+def map(remote: QuestionRemoteDTO, shuffle: List[String] => IO[List[String]]): IO[Question] = {
+  val allAnswers = remote.incorrectAnswers.appended(remote.correctAnswer)
+  shuffle(allAnswers).map( shuffledAnswers =>
+    Question(
+      text = remote.question.text,
+      possibleAnswers = shuffledAnswers,
+      correctAnswer = remote.correctAnswer
+    )
+  )
 }
--- src/test/scala/networking/QuestionRemoteMappingTests.scala
+++ src/test/scala/networking/QuestionRemoteMappingTests.scala
@@ -3,6 +3,8 @@ package networking
 
 import org.scalatest.funsuite.AnyFunSuite
 import domain.Question
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
 
 class QuestionRemoteMappingTests extends AnyFunSuite {
   test("No incorrect answers in dto") {
@@ -15,9 +17,34 @@ class QuestionRemoteMappingTests extends AnyFunSuite {
     )
     val model = Question(
       text = "some question",
-      possibleAnswers = List("correct answer"),
+      possibleAnswers = List("correct answer shuffled"),
       correctAnswer = "correct answer"
     )
-    assert(map(dto) == model)
+    val result = map(dto, shuffle).unsafeRunSync()
+
+    assert(result == model)
+  }
+
+  test("There are incorrect answers in dto") {
+    val textDto = QuestionTextRemoteDTO(text = "some question")
+    val dto = QuestionRemoteDTO(
+      id = "any",
+      question = textDto,
+      correctAnswer = "correct answer",
+      incorrectAnswers = List("incorrect 0", "incorrect 1", "incorrect 2")
+    )
+    val result = map(dto, shuffle).unsafeRunSync()
+
+    assert(result.text == "some question")
+    assert(result.correctAnswer == "correct answer")
+    assert(result.possibleAnswers.contains("correct answer shuffled"))
+    assert(result.possibleAnswers.contains("incorrect 0 shuffled"))
+    assert(result.possibleAnswers.contains("incorrect 1 shuffled"))
+    assert(result.possibleAnswers.contains("incorrect 2 shuffled"))
+    assert(result.possibleAnswers.length == 4)
+  }
+
+  private def shuffle(list: List[String]): IO[List[String]] = {
+    IO.pure(list.map(_ + " shuffled"))
   }
 }
